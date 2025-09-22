53f6a38a675aee5b837cec2a0432d198288ff0a8
maryvi <maryvi.jobfinder@gmail.com>
2025-09-22 15:14:59 +0100

    ex08

--- C00/ex08/ft_print_combn.c
+++ C00/ex08/ft_print_combn.c
@@ -1,4 +1,22 @@
 #include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+int	ft_fact(int n)
+{
+	int	fact;
+
+	if ( n < 0 )
+		return (0);
+	fact = 1;
+	while ( n > 1 )
+	{
+		fact *= n;
+		n--;
+	}
+	return (fact);
+}
+
 
 void	ft_putnbr(int nb)
 {
@@ -15,6 +33,88 @@ void	ft_putnbr(int nb)
 	write(1, &"0123456789"[n % 10], 1);
 }
 
+int	ft_size(int k)
+{
+	int	n;
+	int	size;
+
+	n = 10;
+	size = ft_fact(n) / (ft_fact(n - k) * ft_fact(k));
+	return (size);
+}
+
+int	*ft_combn(int n)
+{
+	int	size;
+	int	i;
+	int	j;
+	int	k;
+	int	*numbers;
+	int	*previous_numbers;
+	int	previous_size;
+
+	size = ft_size(n);
+	numbers = malloc(size * sizeof(int));
+	printf("pass malloc\n");
+	i = 0;
+	j = 0;
+	if (n == 1)
+	{
+		while (i < size)
+		{
+			numbers[i] = i;
+			i++;
+		}
+	}
+	if (n == 2)
+	{
+		previous_size = ft_size(n - 1);
+		previous_numbers = ft_combn(n - 1); 
+		i = 0;
+		while ( i < previous_size )
+		{
+			k = 0;
+			while (k <= 9 && j < size)
+			{
+				if ( k <= previous_numbers[i] )
+					k = previous_numbers[i] + 1;
+				numbers[j] = previous_numbers[i] * 10 + k;
+				k++;
+				j++;
+			}
+			i++;			
+		}
+	}
+	return (numbers);
+}
+
+int	main(void)
+{
+	int	*numbers;
+	int	i;
+	int	size;
+	int	n;
+	int	first_number;
+
+	first_number = 1;
+	n = 2;
+	i = 0;
+	size = ft_size(n);
+	printf("size: %d\n", size);
+	numbers = ft_combn(n);
+	printf("pass numbers");
+	while (i < size)
+	{	
+		if (! first_number)
+			write (1, ", ", 2);
+		ft_putnbr(numbers[i]);
+		i++;
+		first_number = 0;	
+	}
+	write (1, "\n", 1);
+	free(numbers);
+	return (0);
+}
 void	ft_print_combn(int n)
 {
 	int	i;
@@ -55,11 +155,5 @@ void	ft_print_combn(int n)
 	}
 }
 
-int	main(void)
-{
-	ft_print_combn(3);
-	write (1, "\n", 1);
-	return (0);
-}
 
 
