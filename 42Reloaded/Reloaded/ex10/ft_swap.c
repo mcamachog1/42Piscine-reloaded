58c44e41c1b6ecfbad7f37c3e7b2f1e5d64d4e20
maryvi <mcamachog@gmail.com>
2025-10-08 22:28:37 +0100

    reloaded first commit

---
+++ 42Reloaded/Reloaded/ex10/ft_swap.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_swap.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: macamach <mcamach@student.42porto.com      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/08 09:14:29 by macamach          #+#    #+#             */
+/*   Updated: 2025/10/08 09:30:47 by macamach         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+void	ft_swap(int *a, int *b)
+{
+	int	temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+int	main(void)
+{
+	int	a;
+	int	b;
+
+	a = 10;
+	b = 99;
+	printf("a:%i; b:%i\n", a, b);
+	ft_swap(&a, &b);
+	printf ("a:%i; b:%i\n", a, b);
+	return (0);
+}
---
+++ 42Reloaded/Reloaded/ex14/ft_sqrt.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sqrt.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: macamach <mcamach@student.42porto.com      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/08 15:14:19 by macamach          #+#    #+#             */
+/*   Updated: 2025/10/08 15:22:59 by macamach         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int ft_sqrt(int nb)
+{
+	int	n;
+	
+	n = 1;
+	if (nb == 1)
+		return (1);
+	while (n <= nb/2)
+	{
+		if (n * n == nb)
+			return (n);
+		else
+			n++;
+	}
+	return (0);
+}
+
+int	main(void)
+{
+	printf("sqrt(1)=%d\n", ft_sqrt(1));
+	printf("sqrt(0)=%d\n", ft_sqrt(0));
+	printf("sqrt(4)=%d\n", ft_sqrt(4));
+	printf("sqrt(121)=%d\n", ft_sqrt(121));
+	printf("sqrt(1000001)=%d\n", ft_sqrt(1000001));
+	printf("sqrt(32035600)=%d\n", ft_sqrt(32035600));
+	return (0);
+}
